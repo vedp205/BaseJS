05cf48f1d3eb15ef4cf139cb2e48900895c29194
vedp205 <vedp68986@gmail.com>
2024-06-02 21:46:27 +0530

    praticeing code

---
+++ Pra1.js
@@ -0,0 +1,29 @@
+/*console.log("Qs1. Get user to input a number using prompt(Enter a number:). Check if the number is a multiple of 5 or not.")
+let numb = prompt("Enter a number");
+if  (numb%5==0){
+    console.log(numb,"is multiple by 5");
+}
+else{
+    console.log(numb,"is multiple not by 5");
+}*/
+console.log("Qs2. Write a code which can give grades to students according to their scores:")
+let len =prompt("Enter the number");
+
+if (len<=99 && len>=90){
+    console.log("A")
+}
+else if (len<=89 && len>=80 ){
+    console.log("B")
+}
+else if (len<=79 && len>=70){
+    console.log("C");
+}
+else if (len<=69 && len>=60 ){
+    console.log("D");
+}
+else if (len<=59 && len>=50){
+    console.log("E")
+}   
+else if (len>=49 && len<=0){
+    console.log("F")
+}
