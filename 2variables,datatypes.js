4ceb94b5a163e5dbced160148bcd138d9389f31f
vedp205 <vedp68986@gmail.com>
2024-06-02 11:52:16 +0530

    add a folder

---
+++ 2variables,datatypes.js
@@ -0,0 +1,38 @@
+// Variables
+fullname="Ved patel";
+console.log(fullname);
+price=90 ;
+console.log(price);
+
+x=null;
+console.log(x)
+
+y=undefined;
+console.log(y)
+
+isfollow=true;
+console.log(isfollow); 
+
+let fname ="ved";
+let age =20;
+console.log(fname);
+console.log(age);
+
+a=10;
+console.log(a);
+
+// Datatypes
+// primitive
+bigit=BigInt("456123");
+console.log(bigit)
+sym=Symbol("hello.");
+console.log(sym)
+
+
+// non primitive
+
+const st={
+    name:"abc",Number:"334",marks:"87",pass:"true"
+};
+
+console.log(st["Number"])
