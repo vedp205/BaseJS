2f4f330c559affbd7ba530a2871e8f992087df15
vedp205 <vedp68986@gmail.com>
2024-06-23 18:24:47 +0530

    add SPS 25% com logic remaining

---
+++ Projects/stone paper sizzor/script.js
@@ -0,0 +1,19 @@
+const stone_butn = document.getElementById("stone_butn");
+const paper_butn = document.getElementById("paper_butn");
+const scissor_butn = document.getElementById("scissor_butn");
+const score_of_user = document.getElementById("score_of_user");
+const score_of_comp = document.getElementById("score_of_comp");
+
+// let com_random = () => {
+//     return Math.floor(Math.random()*alphabet.length ) ;
+// }
+
+// function createRandomString(length) {
+//     const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     let result = "";
+//     for (let i = 0; i < length; i++) {
+//         result += chars.charAt(Math.floor(Math.random() * chars.length));
+//     }
+//     return result;
+// }
+// console.log(createRandomString(1));
